4c669e1cb816e3d58b19a21f3cc57cfa37bf4954
ninachen <cnina9729@gmail.com>
2022-12-20 18:18:41 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask,render_template,request,redirect
+
+app = Flask(__name__)
+#路由
+
+
+@app.route('/', methods = ['GET','POST'])#要去访问的路径/
+def login():#这边实现登陆的逻辑
+    print() #request 对象可以拿到前端给的所有数据
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        print('data from server : ',username,password)
+
+
+        return redirect('/admin')
+    return render_template('login.html')
+
+@app.route('/admin')
+def admin():
+
+    return render_template('admin.html')
+
+@app.route('/bme')
+def bme():
+
+    return render_template('bme.html')
+
+@app.route('/eg')
+def contact():
+
+    return render_template('example.html')
+
+
+
+if __name__ =='__main__':
+    app.run()
+
